31f73dfdc8b93f22e23cc0c002cff4f966250adb
uchman005 <obiefunamarcel@gmail.com>
2020-10-17 04:13:20 +0100

    I added a route for Madam Sauce to add new food to her collection which makes the food immediately available to be displayed, although it only persists through the server session

--- index.js
+++ index.js
@@ -4,14 +4,35 @@ const bodyParser = require("body-parser");
 const port = 3000;
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
+let foodDisplayArr = ["Rice", "Beans", "Akpu", "Okpa"];
+// this is the array from which Sauce displays available food
 app.get("/", (req, res) => {
-    res.send("We are Sauce kitchen");
+foodDisplayArr.forEach((food)=>{
+    console.log(food);
+//A simple loop that goes over the display array and logs available food
+});
+    
+
+res.end();
+
 });
 
+app.get("/add", (req, res) => {
+    // Route created for Madam Sauce to add new food to her store
+  res.send("<form action='/add' method='post'><input type='text' name='newFood' placeholder='add new food'><input type='number' name='newPrice' placeholder='price of food'><input type='number' name='newQuantity' placeholder='No of plates'><button type='submit'>Add</button></form> ");
+ });
 
-
-
+app.post("/add", (req, res) => {
+    let newFood = req.body.newFood;
+    let newPrice = Number(req.body.newPrice);
+    let newQuantity = Number(req.body.newQuantity);
+ 
+    foodDisplayArr.push(newFood);
+    res.redirect('/');
+  
+});
 
 
 app.listen(port,()=>{
